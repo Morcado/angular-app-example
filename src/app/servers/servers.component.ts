32fa7c421918423d25133cf534ae8974049e79ff
morcado <gposcarr18@gmail.com>
2022-09-07 13:40:03 -0500

    seleccion de componentes de distinta forma

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector por atributo
+  // selector: '[app-servers]',
+  // selector por clase
+  // selector: '.app-servers',
   selector: 'app-servers',
   template: `<app-server></app-server>
     <app-server></app-server>`,
